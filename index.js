a4e660f77b7cb01bae9f0ee7f6e7bb3036bf710f
fairyly <fairyilys@gmail.com>
2018-03-01 15:40:22 +0800

    add user model and login/logout

--- index.js
+++ index.js
@@ -32,8 +32,30 @@ app.use(session({
 // flash 中间件，用来显示通知
 app.use(flash())
 
+// 处理表单及文件上传的中间件 顺序很重要,要加在路由前,不然会报错
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'), // 上传文件目录
+  keepExtensions: true// 保留后缀
+}))
+
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
+
 // 路由
 routes(app)
+
+
 
 // 监听端口，启动程序
 app.listen(config.port, function () {
