56c680fa6c5b77de6a2ebf6a0234846587636bb4
fairyly <fairyilys@gmail.com>
2018-03-01 10:17:57 +0800

    add node blog base struct demo

---
+++ routes/signout.js
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 2018/3/1.
+ */
+const express = require('express')
+const router = express.Router()
+
+const checkLogin = require('../middlewares/check').checkLogin
+
+// GET /signout 登出
+router.get('/', checkLogin, function (req, res, next) {
+  res.send('登出')
+})
+
+module.exports = router
